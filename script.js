5c3a05718903c5cbd32c64ac816c78be58f5470f
capXyl <67283208+capXyl@users.noreply.github.com>
2020-09-16 15:10:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+// QR Library Documentation: https://github.com/LazarSoft/jsqrcode/blob/master/README
+
+const video = document.createElement("video");
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
+
+const qrResult = document.getElementById("qr-result");
+const outputData = document.getElementById("outputData");
+const btnScanQR = document.getElementById("btn-scan-qr");
+const imgInput = document.getElementById("img-input-qr");
+const btnCancelScan = document.getElementById("cancel-scan");
+const header1 = document.getElementById("header-1");
+const header2 = document.getElementById("header-2");
+
+//const hideClass = document.getElementsByClassName("hide-during-scan");
+
+let scanning = false;
+
+qrcode.callback = res => {
+  if (res) {
+    outputData.innerText = res;
+    scanning = false;
+
+    //if (String(res).match(/123/)) {console.log("matched")}
+
+    if (video.srcObject) {
+      video.srcObject.getTracks().forEach(track => {
+        track.stop();
+      });
+    }
+
+    qrResult.hidden = false;
+    canvasElement.hidden = true;
+    btnScanQR.hidden = false;
+    imgInput.hidden = false;
+    header1.innerText = "Click the image to start";
+    header2.hidden = false;
+    btnCancelScan.hidden = true;
+
+    //for (let i = 0; i < hideClass.length; i++) {
+    //  hideClass[i].hidden = false;
+    //}
+  }
+};
+
+btnScanQR.onclick = () => {
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      scanning = true;
+
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      imgInput.hidden = true;
+      header1.innerText = "Scanning...";
+      header2.hidden = true;
+      btnCancelScan.hidden = false;
+
+      //for (let i = 0; i < hideClass.length; i++) {
+      //  hideClass[i].hidden = true;
+      //}
+
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    })
+    .catch(err => console.log(err)
+  );
+};
+
+btnCancelScan.onclick = () => {
+  outputData.innerText = "Scan canceled by user.";
+  scanning = false;
+
+  if (video.srcObject) {
+    video.srcObject.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+
+  qrResult.hidden = false;
+  canvasElement.hidden = true;
+  btnScanQR.hidden = false;
+  imgInput.hidden = false;
+  header1.innerText = "Click the image to start";
+  header2.hidden = false;
+  btnCancelScan.hidden = true;
+};
+
+function tick() {
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+  scanning && requestAnimationFrame(tick);
+}
+
+function scan() {
+  try {
+    qrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
+  }
+}
+
+function handleFiles(file) {
+	for (let i = 0; i < file.length; i++) {
+    var reader = new FileReader();
+    reader.onload = (function(theFile) {
+      return function(e) {
+        qrcode.decode(e.target.result);
+      };
+    })
+    (file[i]);
+    reader.readAsDataURL(file[i]);	
+  }
+}
